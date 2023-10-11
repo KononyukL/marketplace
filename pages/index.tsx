2655333dbbe0ee2b5ccf451850dc5b650ef22318
KononyukL <liliashpak97@gmail.com>
2023-10-11 15:20:08 +0300

    fix bild error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
-import { Inter } from "next/font/google";
 import { HomePage } from "@/pages/home";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
