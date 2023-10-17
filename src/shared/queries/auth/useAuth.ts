56cf33f0d3186951ab86bfe35d50612e2aedd48d
KononyukL <liliashpak97@gmail.com>
2023-10-17 14:38:09 +0300

    fix error

--- src/shared/queries/auth/useAuth.ts
+++ src/shared/queries/auth/useAuth.ts
@@ -1,12 +1,12 @@
-import {selectAuth} from "@/shared/store/auth";
-import {useAppSelector} from "@/shared/store/hooks";
-import {axiosInstance} from "@/shared/api/config";
+import { selectAuth } from "@/shared/store/auth";
+import { useAppSelector } from "@/shared/store/hooks";
+import { axiosInstance } from "@/shared/api/config";
 
 export const useAuth = () => {
   const { auth } = useAppSelector(selectAuth);
   axiosInstance.interceptors.request.use((config) => {
-    if (auth?.token) {
-      config.headers.Authorization = `Bearer ${auth.token}`;
+    if (auth?.accessToken) {
+      config.headers.Authorization = `Bearer ${auth.accessToken}`;
     }
 
     return config;
