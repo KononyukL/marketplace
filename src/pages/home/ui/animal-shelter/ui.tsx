7975a979c5041d9c56195386f8c3c76f6b5cc375
sheen <extezzyren@gmail.com>
2023-11-13 22:33:38 +0200

    chore: add todo for extending button styles

--- src/pages/home/ui/animal-shelter/ui.tsx
+++ src/pages/home/ui/animal-shelter/ui.tsx
@@ -14,6 +14,9 @@ export const AnimalShelter = () => {
             Реєструйтеся та прилаштуйте улюбленців в добрі руки!
           </p>
         </div>
+        {/* TODO: Replace 'max-w-button' className with a new size option in the Button component's 'size' prop
+            Implement additional size style within the Button component to maintain styling consistency 
+            and remove direct style/class overrides in usage */}
         <Button className="max-w-button">
           <Link href="/registration"> Зареєструватись</Link>
         </Button>
