342cb824ae496d39c7a8b965c61d30ca987073cf
KononyukL <liliashpak97@gmail.com>
2023-12-06 17:07:47 +0200

    feat: install and add svgr

--- next.config.js
+++ next.config.js
@@ -2,6 +2,28 @@
 const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   i18n,
+
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+        },
+        {
+          loader: "file-loader",
+          options: {
+            name: "static/[path][name].[ext]",
+          },
+        },
+      ],
+      type: "javascript/auto",
+      issuer: {
+        and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
+      },
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
