224fa64869ae5c39ab7fd434e31ab35b6267f359
KononyukL <liliashpak97@gmail.com>
2023-12-08 14:40:24 +0200

    fix: delete not needed function

--- src/pages/home/ui/top-announcement/ui.tsx
+++ src/pages/home/ui/top-announcement/ui.tsx
@@ -2,11 +2,8 @@ import { useGetCategoriesFavoriteTags } from "@/shared/queries/categories-favori
 import { useState } from "react";
 import { Advertisement } from "@/shared/ui/advertisement";
 import { ButtonTags } from "@/shared/ui/buttons/ui/button-tags";
-import { type ICategoriesFavoriteTags } from "@/shared/api/categories-favorite-tags/types";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
-import { Button } from "@/shared/ui";
-import { SVGRS } from "@/shared/config";
 
 export const TopAnnouncement = () => {
   const { locale } = useRouter();
@@ -18,20 +15,12 @@ export const TopAnnouncement = () => {
     setCategoryId(categoryId);
   };
 
-  const all: ICategoriesFavoriteTags = {
-    category_id: 0,
-    lang_code: data?.[0].lang_code || "",
-    tag_title: t("all-advantages"),
-  };
-
-  const allTags = [all, ...(data || [])];
-
   return (
     <div className="m-auto max-w-main p-14 text-black">
       <h2 className="mb-12 text-4xl font-medium">{t("top")}</h2>
       <div className="flex flex-col gap-8">
         <div className="flex gap-2">
-          {allTags?.map((tag) => (
+          {data?.map((tag) => (
             <ButtonTags
               onClick={onClick(tag.category_id)}
               key={tag.category_id}
@@ -42,11 +31,6 @@ export const TopAnnouncement = () => {
           ))}
         </div>
         <Advertisement />
-      </div>
-      <div className="mt-10 flex justify-center">
-        <Button size="xl" variant="outline" endAdornment={<SVGRS.Plus />}>
-          {t("see-more")}
-        </Button>
       </div>
     </div>
   );
