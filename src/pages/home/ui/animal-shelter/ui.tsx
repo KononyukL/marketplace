5cddaf0e80a1f32fb886054281ff685b991ebbc1
KononyukL <liliashpak97@gmail.com>
2023-12-20 17:35:48 +0200

    fix: change style components

--- src/pages/home/ui/animal-shelter/ui.tsx
+++ src/pages/home/ui/animal-shelter/ui.tsx
@@ -7,7 +7,7 @@ export const AnimalShelter = () => {
   const { t } = useTranslation("common");
 
   return (
-    <div className="m-auto max-w-main p-14 text-black">
+    <div className="m-auto mb-44 max-w-main p-14 text-black">
       <div className="border-borede-2 max-w-primary  flex justify-between rounded-lg border-2 border-solid bg-white pb-12 pl-14 pr-14 pt-12 text-black">
         <div className=" flex flex-col justify-center gap-10">
           <div>
--- src/shared/ui/article/ui.tsx
+++ src/shared/ui/article/ui.tsx
@@ -17,31 +17,36 @@ export const Article = ({ date, time, title, text }: IArticle) => {
   const { t } = useTranslation("home");
 
   return (
-    <div className="w-full max-w-how-it-work text-black ">
-      <div className=" flex  flex-col rounded-lg  bg-white ">
+    <div className="max-w-how-it-worktext-black shadow-box w-full">
+      <div className=" flex  flex-col rounded-lg  bg-white p-4 ">
         <div>
           <Image
-            className="max-w-full rounded-b-none"
+            className="max-w-fullrounded-b-none h-[177px] w-[283px]"
             src="/images/animal-shelter-image.png"
             alt="foto"
-            width={315}
-            height={175}
+            width={283}
+            height={177}
           />
         </div>
-        <div className=" w-full rounded-b-lg border border-solid border-additional px-4 pb-6 pt-3">
-          <div className="mb-2 flex gap-3 text-sm text-text-3">
+        <div className=" w-full  rounded-b-lg ">
+          <div className="mt-3 flex gap-2 text-additional">
+            <p className="rounded-tags-2 bg-tags px-2">котики</p>
+            <p className="rounded-tags-2 bg-tags px-2">котики</p>
+            <p className="rounded-tags-2 bg-tags px-2">котики</p>
+          </div>
+          <div className="mb-2 mt-2 flex gap-3 text-sm text-text-3">
             <p>{dateTimeFormat(date, locale)}</p>
             <p>
               {t("articles.read")} {time} Хв
             </p>
           </div>
           <div className="flex flex-col gap-2">
-            <h3 className=" h-title text-xl font-medium">{title}</h3>
-            <p className="min-h-article max-w-[283px]">
+            <h3 className=" h-title font-medium">{title}</h3>
+            <p className="min-h-article max-w-[283px] text-sm">
               {text.length > 150 && text.slice(0, 150) + "..."}
             </p>
             <div className="mr-10 flex items-end justify-end">
-              <Link className="hover:text-primary-hover" href="#">
+              <Link className="hover:text-primary-hover" href="/blog">
                 {t("articles.read-more")}
               </Link>
             </div>
