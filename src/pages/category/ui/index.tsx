834f5d03c23cbecf8a9f23eba27aaa4fc756c1ab
Zubko Alla <zakalinovka@gmail.com>
2024-01-22 17:19:48 +0200

    fix: change name files
    don't change path to svg in bredcrumbs

--- src/pages/category/ui/index.tsx
+++ src/pages/category/ui/index.tsx
@@ -1,30 +1,24 @@
 import { useRouter } from "next/router";
 import { useGetCategories } from "@/shared/queries/categories";
-import { TestPage } from "./testPage";
+import { TestHeader } from "./test-header";
 import { Breadcrumbs } from "@/shared/ui/breadcrumbs";
 
 export const Category = () => {
   const { query, asPath } = useRouter();
-  console.log(asPath);
+
   const selected = asPath.split("/");
-  const { data, status } = useGetCategories();
-  // const segmentName: [] = [];
-  // data?.forEach((el) => {
-  //   segmentName.push({ id: el.id, content: el.title });
-  // });
-  // console.log(segmentName);
+  const { data } = useGetCategories();
+
   const categoryId = query?.id as string;
 
   const categoryItem = data?.find((category) => category.id === +categoryId);
 
-  if (status === "success") {
-    return (
-      <>
-        {categoryItem && (
-          <Breadcrumbs crumbs={selected} categoryItem={categoryItem} />
-        )}
-        <TestPage />
-      </>
-    );
-  }
+  return (
+    <>
+      {categoryItem && (
+        <Breadcrumbs crumbs={selected} categoryItem={categoryItem} />
+      )}
+      <TestHeader />
+    </>
+  );
 };
--- src/pages/category/ui/test-header/ui.tsx
+++ src/pages/category/ui/test-header/ui.tsx
@@ -3,12 +3,12 @@ import { useTranslation } from "next-i18next";
 import { Button } from "@/shared/ui";
 import Link from "next/link";
 
-export const TestPage = () => {
+export const TestHeader = () => {
   const { t } = useTranslation(["categories"]);
   return (
     <>
       <div className=" relative mx-14 h-64 max-w-main px-8 pb-10 pt-12 text-black">
-        <div className="text-title-2 max-w-test-text relative z-10 flex flex-col items-start gap-3 break-normal ">
+        <div className="relative z-10 flex max-w-test-text flex-col items-start gap-3 break-normal text-title-2 ">
           <h3 className="text-[28px] leading-snug">{t("test.title")}</h3>
           <p className="pb-2 text-base leading-relaxed">
             {t("test.text-title")}
