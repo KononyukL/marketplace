ff1b829397fc93d3e54dc4f3318acef42b4c97b6
sheen <extezzyren@gmail.com>
2024-04-07 18:07:41 +0300

    fix(auth): move useAuth hook to base layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,8 @@
 import { HomePage } from "@/pages/home";
-import { useAuth } from "@/shared/queries/auth/useAuth";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { type GetStaticProps } from "next";
 
 export default function Home() {
-  // TODO move it to index file
-  useAuth();
-
   return (
     <>
       <HomePage />
--- src/widgets/layouts/base-layout/ui.tsx
+++ src/widgets/layouts/base-layout/ui.tsx
@@ -5,11 +5,13 @@ import { useRouter } from "next/router";
 import { Roboto_Flex } from "next/font/google";
 import { Footer } from "@/widgets/footer";
 import { ButtonScroll } from "@/shared/ui/buttons/ui/button-scroll";
+import { useAuth } from "@/shared/queries/auth/useAuth";
 
 const PATH_WITHOUT_LAYOUT = ["/login", "/registration"];
 
 const robotoFlex = Roboto_Flex({ subsets: ["latin", "cyrillic"] });
 export const BaseLayout = ({ children }: PropsWithChildren) => {
+  useAuth();
   const router = useRouter();
 
   if (PATH_WITHOUT_LAYOUT.includes(router.pathname)) {
