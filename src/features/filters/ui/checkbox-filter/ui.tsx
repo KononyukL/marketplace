2ba65dc17f5520f0a7bd4e6d38ac30de6b10e124
KononyukL <liliashpak97@gmail.com>
2024-04-11 16:40:57 +0300

    feat: create components: checkbox, accordion and gender

---
+++ src/features/filters/ui/checkbox-filter/ui.tsx
@@ -0,0 +1,41 @@
+export interface ICheckboxPFilter {
+  disabled?: boolean;
+  defaultChecked?: boolean;
+  id: string;
+  label: string;
+}
+
+export const CheckboxFilter = (props: ICheckboxPFilter) => {
+  return (
+    <div className="flex w-full items-center gap-2">
+      <input
+        className="
+        disabled:border-steel-400
+        disabled:bg-steel-400 peer peer relative mt-1 h-6 w-6 shrink-0 appearance-none rounded-sm border-2
+        border-border-2 bg-white checked:border-0 checked:bg-additional
+        focus:outline-none
+      "
+        type="checkbox"
+        {...props}
+      />
+      <svg
+        className="pointer-events-none absolute  ml-1 mt-1  hidden h-4 w-4 stroke-white outline-none peer-checked:block"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <polyline points="20 6 9 17 4 12"></polyline>
+      </svg>
+      <label
+        className="peer-checked:font-medium peer-checked:text-black"
+        htmlFor={props.id}
+      >
+        {props.label}
+      </label>
+    </div>
+  );
+};
---
+++ src/shared/ui/accordion/ui.tsx
@@ -0,0 +1,52 @@
+import { cn } from "@/shared/lib/cn";
+import { useState } from "react";
+
+interface IAccordion {
+  children: React.ReactNode;
+  size?: "xsm" | "sm";
+  className?: string;
+  title: string;
+}
+
+export const Accordion = ({ children, size, className, title }: IAccordion) => {
+  const [accordionOpen, setAccordionOpen] = useState(false);
+  return (
+    <div
+      className={cn("w-full bg-white py-2 ", className, {
+        "max-w-filter": size === "xsm",
+      })}
+    >
+      <button
+        onClick={() => setAccordionOpen(!accordionOpen)}
+        className="flex w-full items-center justify-between"
+      >
+        <span>{title}</span>
+        <svg
+          className={`h-4 w-4 transition-transform ${
+            accordionOpen ? " transform" : ""
+          }`}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="none"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d={accordionOpen ? "M19 14l-7-7-7 7" : "M5 6l7 7 7-7"}
+          />
+        </svg>
+      </button>
+      <div
+        className={`grid overflow-hidden border-b border-b-text-2 py-3 text-sm text-slate-600 transition-all  duration-300 ease-in-out ${
+          accordionOpen
+            ? "grid-rows-[1fr] opacity-100"
+            : "grid-rows-[0fr] opacity-0"
+        }`}
+      >
+        <div className="overflow-hidden"> {children}</div>
+      </div>
+    </div>
+  );
+};
