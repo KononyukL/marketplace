d4457eea1d22663fd52617d2f536642a7da57728
Olya <ponomaryova.olya.y@gmail.com>
2024-05-22 11:43:06 +0200

    fix: add padding to TipsInfo component.

--- src/pages/announcement/ui/tips-info.tsx
+++ src/pages/announcement/ui/tips-info.tsx
@@ -16,9 +16,12 @@ export const TipsInfo = () => {
         {t("tips-info.header")}
       </h3>
       <div
-        className={cn("text-text-5 overflow-hidden border-t border-text-2", {
-          "max-h-[230px]": isCollapsed,
-        })}
+        className={cn(
+          "text-text-5 overflow-hidden border-t border-text-2 px-[72px] py-8",
+          {
+            "max-h-[230px]": isCollapsed,
+          },
+        )}
       >
         <TipComponent />
       </div>
