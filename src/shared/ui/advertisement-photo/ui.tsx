5c0d418ac1ef40b17a81233e96810dd860e25590
KononyukL <liliashpak97@gmail.com>
2024-06-07 16:50:32 +0300

    fix: changed the component AdvertisementPhotos to one photo on all ads

--- src/shared/ui/advertisement-photo/ui.tsx
+++ src/shared/ui/advertisement-photo/ui.tsx
@@ -1,18 +1,19 @@
 import Image from "next/image";
 import { type IImage } from "@/shared/api/advertisement-favorite/types";
+import { Icons } from "@/shared/config";
 
 interface IAdvertisementPhotos {
   img: IImage[];
 }
 
 export const AdvertisementPhotos = ({ img }: IAdvertisementPhotos) => {
-  const [firstImg, secondImg, thirdImg] = img;
+  const [firstImg] = img;
 
   if (!img.length) {
     return (
       <div className="flex max-w-advertisement-photo flex-1 items-center justify-center">
         <Image
-          className="rounded-lg"
+          className="rounded"
           src="/images/image-not-photo.png"
           alt="foto"
           width={390}
@@ -22,70 +23,20 @@ export const AdvertisementPhotos = ({ img }: IAdvertisementPhotos) => {
     );
   }
 
-  switch (img.length) {
-    case 1:
-      return (
-        <div className="flex h-image-advertisement max-h-photo-2 max-w-advertisement-photo items-center justify-center overflow-hidden rounded-lg bg-disable">
-          <Image
-            className=" rounded-lg"
-            objectFit="contain"
-            src={firstImg.url}
-            alt="foto"
-            width={390}
-            height={360}
-          />
-        </div>
-      );
-    case 2:
-      return (
-        <div className="flex  max-w-advertisement-photo flex-col items-center justify-center gap-2 overflow-hidden rounded-lg">
-          <Image
-            className="h-image max-w-advertisement-photo rounded-lg object-cover"
-            src={firstImg.url}
-            alt="foto"
-            width={390}
-            height={400}
-          />
-          <Image
-            className="h-image max-w-advertisement-photo rounded-lg object-cover"
-            src={secondImg.url}
-            alt="foto"
-            width={390}
-            height={390}
-          />
-        </div>
-      );
-    default:
-      return (
-        <div className="flex h-image-advertisement max-w-advertisement-photo flex-col items-center justify-center gap-2 ">
-          <div className="relative max-h-photo overflow-hidden rounded-lg">
-            <Image
-              className="h-image object-cover "
-              src={firstImg.url}
-              alt="foto"
-              width={390}
-              height={158}
-            />
-          </div>
-          <div className="flex gap-4 ">
-            <Image
-              className="max-h-image-2 flex-initial rounded-lg object-cover"
-              src={secondImg.url}
-              objectFit="contain"
-              alt="foto"
-              width={390}
-              height={158}
-            />
-            <Image
-              className="max-h-image-2 flex-initial rounded-lg object-cover"
-              src={thirdImg.url}
-              objectFit="contain"
-              alt="foto"
-              width={390}
-              height={158}
-            />
-          </div>
-        </div>
-      );
-  }
+  return (
+    <div className="relative flex h-image-advertisement max-h-photo-2 max-w-advertisement-photo items-center justify-center overflow-hidden rounded bg-disable">
+      <Image
+        className="rounded"
+        objectFit="contain"
+        src={firstImg.url}
+        alt="foto"
+        width={390}
+        height={360}
+      />
+      <div className="img-count absolute bottom-1 right-1 inline-flex w-[60px] items-center justify-center gap-2 rounded bg-white p-2 text-title">
+        <Icons.CameraSmall />
+        {img.length}
+      </div>
+    </div>
+  );
 };
