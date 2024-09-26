dc25eb9d460ab9af36450d699a53f608f2b5dc41
Zubko Alla <zakalinovka@gmail.com>
2024-09-26 15:01:10 +0300

    style: :bug: change styles in cards, fix bug "chevron" to filter

--- src/shared/ui/advertisement-photo/ui.tsx
+++ src/shared/ui/advertisement-photo/ui.tsx
@@ -17,22 +17,23 @@ export const AdvertisementPhotos = ({ img }: IAdvertisementPhotos) => {
           src="/images/image-not-photo.png"
           alt="foto"
           width={390}
-          height={360}
+          height={300}
         />
       </div>
     );
   }
 
   return (
-    <div className="relative flex h-image-advertisement max-h-photo-2 max-w-advertisement-photo items-center justify-center overflow-hidden rounded bg-disable">
+    <div className="relative flex h-image-advertisement max-w-advertisement-photo items-center justify-center overflow-hidden rounded bg-disable">
       <Image
-        className="rounded"
+        className="h-full rounded"
         objectFit="contain"
         src={firstImg.url}
         alt="foto"
         width={390}
-        height={360}
+        height={300}
       />
+
       <div className="img-count absolute bottom-1 right-1 inline-flex w-[60px] items-center justify-center gap-2 rounded bg-white p-2 text-title">
         <Icons.CameraSmall />
         {img.length}
