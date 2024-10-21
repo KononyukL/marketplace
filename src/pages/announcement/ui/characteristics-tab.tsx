4e8b4475df0ac576ded2a5b01b7486168ba414ff
Olya <ponomaryova.olya.y@gmail.com>
2024-10-21 12:39:58 +0200

    fix: add useMemo hook; use id as key

--- src/pages/announcement/ui/characteristics-tab.tsx
+++ src/pages/announcement/ui/characteristics-tab.tsx
@@ -1,4 +1,5 @@
 import { type IAttribute } from "@/shared/api/advertisement-favorite/types";
+import { useMemo } from "react";
 
 export const CharacteristicsTab = ({
   favoriteAttributes,
@@ -7,10 +8,11 @@ export const CharacteristicsTab = ({
   favoriteAttributes: IAttribute[];
   attributes: IAttribute[];
 }) => {
+  const characteristicsData = useMemo(() => [...favoriteAttributes, ...attributes], [favoriteAttributes, attributes]);
   return (
  <div className="flex flex-col gap-3 flex-wrap max-h-characteristics">
-  {[...favoriteAttributes, ...attributes].map((el, index) => (
-    <div key={index} className="w-[50%] flex">
+  {characteristicsData.map((el) => (
+    <div key={el.id} className="w-[50%] flex">
         <span className="text-text-3 w-[35%]">{el.group_title}</span>
         <span className="text-title w-[65%]">{el.title}</span>
       </div>
