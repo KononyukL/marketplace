44a938e5b896c49a8fe7356762f45fb6e4a92e68
sheen <extezzyren@gmail.com>
2024-10-24 00:23:57 +0300

    fix: turn off eslint rule about floating promises

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,9 +13,10 @@ const config = {
         project: path.join(__dirname, "tsconfig.json"),
       },
       rules: {
-        '@typescript-eslint/no-unsafe-assignment': 'off',
-        '@typescript-eslint/no-unsafe-member-access': 'off',
-      }
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+      },
     },
   ],
   parser: "@typescript-eslint/parser",
@@ -33,8 +34,9 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    '@typescript-eslint/no-unsafe-assignment': 'off',
+    "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-floating-promises": "off",
   },
 };
 
